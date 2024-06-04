8d10e09c89cdeaea034d33c52166e1b0918fd352
Abhiraj Kumar <kumarshishu99@gmail.com>
2024-06-04 22:15:12 +0530

    CSS fix

--- Codes/foodie/src/components/body/mindCarousel/CarouselRestaurantList.jsx
+++ Codes/foodie/src/components/body/mindCarousel/CarouselRestaurantList.jsx
@@ -16,9 +16,6 @@ const CarouselRestaurantList = () => {
   
   if(response?.data?.cards?.length > 0){
     const data = response?.data?.cards;
-    console.log('data >>', data)
-    console.log('data.slice(3) >>', data.slice(3))
-
     const headerData = data[0]?.card?.card;
     const resCount = data[1]?.card?.card?.restaurantCount;
     return (
@@ -28,16 +25,18 @@ const CarouselRestaurantList = () => {
               <p className="text-[24px] text-[#3D4152]">{headerData?.description}</p>
           </div>
           <p className="font-[700] text-[30px] py-4 border-t border-gray-200"> {resCount} restaurants to explore</p>
-          <div className="flex flex-wrap gap-8 justify-between">
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-between">
             {
 
                 data.slice(3).map((item) => {
                     const itemInfo = item?.card?.card?.info;
-                    return (
-                        <div key={itemInfo?.id}>
-                            <RestaurantCard restaurant={itemInfo} />
-                        </div>
-                    )
+                    if(itemInfo){
+                        return (
+                            <div className="justify-self-center" key={itemInfo?.id}>
+                                <RestaurantCard restaurant={itemInfo} />
+                            </div>
+                        )
+                    }
                 })
             }
           </div>
--- Codes/foodie/src/components/header/Header.jsx
+++ Codes/foodie/src/components/header/Header.jsx
@@ -1,12 +1,15 @@
+import { NavLink } from "react-router-dom";
 import { hideInMobileView, logo_url, navItemCSS } from "../../utils/header";
 import "./header.css"
 
 const Header = () => {
     return(
         <div className="header flex items-center justify-between px-8 py-6 sticky top-0 z-20 bg-white">
-            <div className="header_left flex items-center cursor-pointer">
-                <img src={logo_url} className="h-[40px]" alt="app_logo" />
-            </div>
+            <NavLink to="/">
+                <div className="header_left flex items-center cursor-pointer">
+                    <img src={logo_url} className="h-[40px]" alt="app_logo" />
+                </div>
+            </NavLink>
             <div className="header_right flex items-center gap-x-8">
                 <div className={`${navItemCSS} ${hideInMobileView}`}>
                     <i className="fa-solid fa-magnifying-glass"></i>
